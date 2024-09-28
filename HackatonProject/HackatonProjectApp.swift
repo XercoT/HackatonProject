bd59b59b382558dd9dabab3d40a3caba45683e7e
XercoT <149633926+XercoT@users.noreply.github.com>
2024-09-28 09:59:40 -0400

    Initial Commit

---
+++ HackatonProject/HackatonProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackatonProjectApp.swift
+//  HackatonProject
+//
+//  Created by Xerco Tchouankeu on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HackatonProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
